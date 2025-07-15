a1ddd5fa6aaf4335c83bf1f534c93c78952bd2da
davidoskky <davidoskky@yahoo.it>
2025-07-15 15:52:14 +0200

    Add Lazy Kennard Stone implementations

--- src/DataSplits.jl
+++ src/DataSplits.jl
@@ -3,5 +3,6 @@ module DataSplits
 export split
 include("core.jl")
 include("strategies/random.jl")
+include("strategies/KennardStone.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
 using Test
 using DataSplits
 include("test_random.jl")
+include("test_kennard_stone.jl")
