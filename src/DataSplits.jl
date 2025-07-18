3aa17d13e8ce4d036bfbe545024af09ab8c1950f
davide-grheco <davide.crucitti@sergas.es>
2025-07-18 15:42:56 +0200

    Added Sphere Exclusion Clustering

--- src/DataSplits.jl
+++ src/DataSplits.jl
@@ -1,6 +1,5 @@
 module DataSplits
 
-
 include("core.jl")
 include("utils.jl")
 include("strategies/random.jl")
@@ -8,11 +7,13 @@ include("strategies/LazyKennardStone.jl")
 include("strategies/KennardStone.jl")
 include("strategies/SPXY.jl")
 include("strategies/OptiSim.jl")
+include("clustering/SphereExclusion.jl")
 
 export split
 export LazyCADEXSplit, LazyKennardStoneSplit, KennardStoneSplit
 export SPXYSplit
 export OptiSimSplit
 export RandomSplit
+export sphere_exclusion
 
 end
